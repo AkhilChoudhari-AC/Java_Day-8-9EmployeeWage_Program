3214bd0c60bb601710f1b5c7dd4f1a3f533c7eb8
Akhil-Choudhari <akhil.choudhari.ac@gmail.com>
2022-07-04 19:24:54 +0530

    REFACTOR: Employee Wages using Switch Case

--- EmpComputation.java
+++ EmpComputation.java
@@ -1,28 +1,28 @@
 package EmployeeWageProgram;
 
 public class EmpComputation {
-    public static final int IS_PRESENT = 1;
-    public static final int IS_PART_TIME = 2;
+    public static final int IS_FULL_TIME = 1;
     public static final int SALARY_PER_HR = 20;
+    public static final int IS_PART_TIME = 2;
 
     public static void employeeAttendance() {
 
-        double empCheck = Math.floor((Math.random() * 10) % 2);
+        int empCheck = (int) Math.floor((Math.random() * 10) % 3);
         int Working_Hr = 0;
-
-        if (empCheck == IS_PRESENT) {
-            Working_Hr = 8;
-        } else if (empCheck == IS_PART_TIME) {
-            Working_Hr = 4;
-        } else {
-            Working_Hr = 0;
+        switch (empCheck) {
+            case IS_FULL_TIME:
+                Working_Hr = 8;
+                break;
+            case IS_PART_TIME:
+                Working_Hr = 4;
+                break;
+            default:
+                Working_Hr = 0;
         }
         double Salary = Working_Hr * SALARY_PER_HR;
-        System.out.println("Emplyee Salary is = " + Salary + "$");
+        System.out.println("Employee Salary is = " + Salary + "$");
     }
-
     public static void main(String[] args) {
         EmpComputation.employeeAttendance();
     }
-
 }
