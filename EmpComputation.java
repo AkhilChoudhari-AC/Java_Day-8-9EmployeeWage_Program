2de02ee1f0a2c67cbc2645de90fde41c36ee0d2a
Akhil-Choudhari <akhil.choudhari.ac@gmail.com>
2022-07-04 19:30:05 +0530

    ADDED: Calculating Wages for Month

--- EmpComputation.java
+++ EmpComputation.java
@@ -4,25 +4,36 @@ public class EmpComputation {
     public static final int IS_FULL_TIME = 1;
     public static final int SALARY_PER_HR = 20;
     public static final int IS_PART_TIME = 2;
+    public static final int NO_OF_DAYS = 20;
 
     public static void employeeAttendance() {
 
-        int empCheck = (int) Math.floor((Math.random() * 10) % 3);
         int Working_Hr = 0;
-        switch (empCheck) {
-            case IS_FULL_TIME:
-                Working_Hr = 8;
-                break;
-            case IS_PART_TIME:
-                Working_Hr = 4;
-                break;
-            default:
-                Working_Hr = 0;
+        int empwage = 0;
+        int Totalempwage = 0;
+
+        for (int day = 0; day < NO_OF_DAYS; day++) {
+            int empCheck = (int) Math.floor((Math.random() * 10) % 3);
+
+            switch (empCheck) {
+                case IS_FULL_TIME:
+                    Working_Hr = 8;
+                    break;
+                case IS_PART_TIME:
+                    Working_Hr = 4;
+                    break;
+                default:
+                    Working_Hr = 0;
+            }
+            empwage = Working_Hr * SALARY_PER_HR;
+            Totalempwage += empwage;
+            System.out.println("Emp Wage :  " + empwage);
         }
-        double Salary = Working_Hr * SALARY_PER_HR;
-        System.out.println("Employee Salary is = " + Salary + "$");
+
+        System.out.println("Total Emp Wage : " + Totalempwage);
     }
     public static void main(String[] args) {
+
         EmpComputation.employeeAttendance();
     }
 }
