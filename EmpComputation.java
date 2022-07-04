3635312e4c588dca648974cf19b19089dc8b6913
Akhil-Choudhari <akhil.choudhari.ac@gmail.com>
2022-07-04 19:11:19 +0530

    ADDED: Daily Employee Wage

--- EmpComputation.java
+++ EmpComputation.java
@@ -1,15 +1,19 @@
 package EmployeeWageProgram;
 
 public class EmpComputation {
+    public static final int IS_PRESENT = 1;
+    public static final int SALARY_PER_HR = 20;
+
     public static void employeeAttendance() {
-        int IS_PRESENT = 1;
         double empCheck = Math.floor((Math.random() * 10) % 2);
+        int Working_Hr = 0;
         if (empCheck == IS_PRESENT) {
-            System.out.println( "Employee is Present" );
-        } else {
-            System.out.println( "Employee is Absent" );
+            Working_Hr = 8;
         }
+        double Salary = Working_Hr * SALARY_PER_HR;
+        System.out.println("Employee Salary is = " + Salary + "$");
     }
+
     public static void main(String[] args) {
         EmpComputation.employeeAttendance();
     }
