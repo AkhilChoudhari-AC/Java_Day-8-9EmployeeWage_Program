24d5060295efc72861e562887c5fe482a9de658c
Akhil-Choudhari <akhil.choudhari.ac@gmail.com>
2022-07-04 19:03:04 +0530

    ADDED: Employee is Present or Absent

--- EmpComputation.java
+++ EmpComputation.java
@@ -3,20 +3,14 @@ package EmployeeWageProgram;
 public class EmpComputation {
     public static void employeeAttendance() {
         int IS_PRESENT = 1;
-
-
         double empCheck = Math.floor((Math.random() * 10) % 2);
-
         if (empCheck == IS_PRESENT) {
-
             System.out.println( "Employee is Present" );
         } else {
             System.out.println( "Employee is Absent" );
         }
     }
-
     public static void main(String[] args) {
         EmpComputation.employeeAttendance();
     }
-
 }
